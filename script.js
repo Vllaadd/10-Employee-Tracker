09c758c774d049108f79fe56578bb1180fca30ea
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-08 05:33:29 -0400

    added prompts and switch method

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql')
-const chalk = require('chalk')
+const chalk = require('chalk');
+const { allowedNodeEnvironmentFlags } = require('process');
+
+
+function mainFunction(){
+    inquirer
+        .prompt({
+            name: 'track',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'Add departmnets',
+                'View departments',
+                'Delete departments',
+                'Add roles',
+                'View roles',
+                'Update roles',
+                'Delete roles',
+                'Add employees',
+                'View employees',
+                'Delete employees'
+            ]
+        })
+        .then(function(answer){
+            switch(userAnswer){
+                case 'Add departments':
+                    addDepartments();
+                    break;
+                case 'View departments':
+                    viewDepartments();
+                    break;
+                case 'Delete departments':
+                    delDepartments();
+                    break;
+                case 'Add roles':
+                    addRoles();
+                    break;
+                case 'View roles':
+                    viewRoles();
+                    break;
+                case 'Update roles':
+                    updateRoles();
+                    break;
+                case 'Delete roles':
+                    delRoles();
+                    break;
+                case 'Add employees':
+                    addEmpl();
+                    break;
+                case 'View employees':
+                    viewEmpl();
+                    break;
+                case 'Delete employees':
+                    delEmpl();
+                    break;
+            }
+        })
+}
